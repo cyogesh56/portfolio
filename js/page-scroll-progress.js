e53253bd55f34e9c0c10cc17ce3605fc5035879c
Yogesh Singh <cyogesh56@gmail.com>
2024-08-02 00:25:00 -0500

    Version 5.6
    Fixed and enhanced a lot of small things that make a difference.

--- js/page-scroll-progress.js
+++ js/page-scroll-progress.js
@@ -1,16 +1,11 @@
 // Page Scroll
-$(function(){$(window).scroll(function(){$('.page-scroll-progress-bar').css({width:getScrollPercent()});});});
+$(function() { $(window).scroll(function() { $('.page-scroll-progress-bar').css({ width: getScrollPercent() }); }); });
 
 // Page Scroll Percent
 function getScrollPercent() {
-    var h = document.documentElement, 
+    var h = document.documentElement,
         b = document.body,
         st = 'scrollTop',
         sh = 'scrollHeight';
-    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100+'%';
+    return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100 + '%';
 }
-
-
-
-
-	
